a6a71450e17890660f46f7614e76a5576d91ab7b
Sebastian Hermosilla Ferreira <87145545+sehermosillaf@users.noreply.github.com>
2025-01-02 23:03:47 -0300

    Agrega cartas restantes 'face cards'

--- js/juego.js
+++ js/juego.js
@@ -1,6 +1,7 @@
 // C => Clubs , D => Diamonds, H => Hearts, S => Spades 
 let deck = [];
 const suits =  ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+const faceCards = ['Jacks', 'Queens','Kings','Aces']
 let playerScore = 0;
 let computerScore = 0;
 
@@ -11,6 +12,13 @@ const createDeck = () => {
             deck.push(i + suit.charAt(0))
         }
     }
+    for (let suit of suits) {
+        for (let faceCard of faceCards) {
+            deck.push(faceCard.charAt(0) + suit.charAt(0))
+        }
+        
+    }
+    console.log(deck);
     
 }
 
