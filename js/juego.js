ca4d9275f226a95131f518dd8049f8c4637feb4a
Sebastian Hermosilla Ferreira <87145545+sehermosillaf@users.noreply.github.com>
2025-01-04 16:01:27 -0300

    Fix: Corrige seleccion de cartas random a la primera carta
    La baraja ya esta revuelta por lo que se deberia seleccionar la primera carta

--- js/juego.js
+++ js/juego.js
@@ -1,23 +1,43 @@
-// C => Clubs , D => Diamonds, H => Hearts, S => Spades 
+
+// C => Clubs , D => Diamonds, H => Hearts, S => Spades
 let deck = [];
-const suits =  ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-const faceCards = ['Jacks', 'Queens','Kings','Aces']
+const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+const faceCards = ["Jacks", "Queens", "Kings", "Aces"];
 let playerScore = 0;
 let computerScore = 0;
 
 const createDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let suit of suits) {
+      deck.push(i + suit.charAt(0));
     }
+  }
+  for (let suit of suits) {
+    for (let faceCard of faceCards) {
+      deck.push(faceCard.charAt(0) + suit.charAt(0));
+    }
+  }
   deck = _.shuffle(deck)
+  console.log(deck);
+};
+// revisar reglas de juego
+// TODO: no deberia ser random la carta ya que la baraja ya esta revuelta.
 const hitCard = () => {
-    let randomIndex  = _.random(0,deck.length)
-    let randomCard = deck[randomIndex]
-    deck.splice(randomIndex,1)
-    console.log(deck);
+  //obtenemos la primera carta de la baraja y la eliminamos
+  let selectedCard = deck[0];
+  deck.splice(selectedCard,1)
+
+  // crear funcion para separar
+  const card = document.createElement("img");
+  card.src = `assets/cartas/${selectedCard}.png`;
+  card.className = "carta"
+  document.getElementById("jugador-cartas").appendChild(card);
+  
     if (deck.length <= 0) {
         window.alert("no quedan cartas en la baraja")
     }
 }
+
+
 
 createDeck()
