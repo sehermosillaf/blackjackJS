ea1dd61007d77eb70d06dcbbe62794b3a839dbe4
Sebastian Hermosilla Ferreira <87145545+sehermosillaf@users.noreply.github.com>
2025-01-05 00:46:08 -0300

    Fix: corrige valor de carta 10
    Estaba tomando el valor del primer caracter
    todo: mejorar solucion

--- js/juego.js
+++ js/juego.js
@@ -1,6 +1,6 @@
-
 // C => Clubs , D => Diamonds, H => Hearts, S => Spades
 let deck = [];
+let value = 0;
 const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
 const faceCards = ["Jacks", "Queens", "Kings", "Aces"];
 const playerCardsElement = document.getElementById("jugador-cartas");
@@ -19,56 +19,82 @@ const createDeck = () => {
       deck.push(faceCard.charAt(0) + suit.charAt(0));
     }
   }
-  deck = _.shuffle(deck)
+  deck = _.shuffle(deck);
 };
 
+const cleanDeck = () => (deck = []);
 
-const cleanDeck = () =>  deck = [];
 const cleanScores = () => {
   playerScore = 0;
-  computerScore =  0;
+  computerScore = 0;
   const playerScoreElement = document.getElementById("player-score");
-  playerScoreElement.textContent="0";
-}
+  playerScoreElement.textContent = "0";
+};
 
 const resetTable = () => {
-  while(playerCardsElement.firstChild) {
-  playerCardsElement.removeChild(playerCardsElement.firstChild)
- }
- cleanDeck();
- createDeck();
- cleanScores();
- console.log(deck);
-}
+  while (playerCardsElement.firstChild) {
+    playerCardsElement.removeChild(playerCardsElement.firstChild);
+  }
+  cleanDeck();
+  createDeck();
+  cleanScores();
+  console.log(deck);
+};
 
 const getCard = () => {
   if (deck.length <= 0) {
-    window.alert("no quedan cartas en la baraja")
-}
+    window.alert("no quedan cartas en la baraja");
+  }
   const selectedCard = deck[0];
-  deck.splice(selectedCard,1);
+  deck.splice(selectedCard, 1);
 
   const card = document.createElement("img");
   card.src = `assets/cartas/${selectedCard}.png`;
-  card.className = "carta"
+  card.className = "carta";
   document.getElementById("jugador-cartas").appendChild(card);
-  updateScores(selectedCard)
-
-}
+  updateScores(selectedCard);
+};
 
 const updateScores = (card) => {
   const playerScoreElement = document.getElementById("player-score");
-  const currentScore = parseInt(playerScoreElement.textContent) || 0
+  const currentScore = parseInt(playerScoreElement.textContent) || 0;
   const newScore = currentScore + cardValue(card);
   playerScoreElement.textContent = newScore;
+  checkBlackJack(newScore)
+  console.log(newScore);
+};
+
+const checkBlackJack = (value) => {
+  if (value == 21) {
+    window.alert('ganaste ' +  value);
+  }
+
+  if (value > 21) {
+    window.alert('te pasaste ' + value);
+    resetTable()
+  }
+
 }
 
-// Todo: revisar valor de los Aces
+// Todo: revisar valor de los Aces y 10s
+// corrige valor 10, tomaba solo el 1 del string con charat 
+// todo: ver como mejorar esta funcion
 const cardValue = (selectedCard) => {
-  const card = selectedCard.charAt(0);
-  return value  =  isNaN(card) ? 10 : card * 1;
-}
-cardValue('2D');
-console.log(value);
+  if(selectedCard.length == 3) {
+    value = 10;
+    return value;
+  }
+  let card = selectedCard.charAt(0);
 
-createDeck()
+  return (value = isNaN(card) ? 10 : card * 1);
+  console.log(value);
+};
+
+
+const stand = () => {
+  
+}
+
+
+cardValue('10D')
+createDeck();
